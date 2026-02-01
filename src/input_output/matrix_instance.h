a1ef1350ae2ba29bda5d726b2d5fa491a03f6570
Marco Perez Padilla <alu0101469348@ull.edu.es>
2026-02-01 00:19:50 +0000

    Fix minor comments and start instances implementation

---
+++ src/input_output/matrix_instance.h
@@ -0,0 +1,25 @@
+/**
+** Universidad de La Laguna
+** Escuela Superior de Ingenieria y Tecnologia
+** Grado en Ingenieria Informatica
+** Asignatura: Diseño y Analisis de Algoritmos
+** Curso: 3º
+** Practica 1: Complejidad Computacional
+** Autor: Marco Pérez Padilla
+** Correo: alu0101469348@ull.edu.es
+** Fecha: 01/02/2026
+
+** Archivo matrix_instance.h: Representa las instancias que son entradas del problema
+**
+** Referencias:
+**      Enlaces de interes
+
+** Historial de revisiones:
+**      01/02/2026 - Creacion (primera version) del codigo
+**/
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+
+#endif
